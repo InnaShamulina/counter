882af6be7551d0e1948e06546284d20d80364d0b
InnaShamulina <innaaf@yandex.ru>
2024-09-25 16:32:09 +0300

    project 1st sprint

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0);
+
+  let maxValue = 5;
+
+
+  const inc = () => {
+    let newValue = ++count
+    if (newValue <= maxValue) {
+    setCount (newValue)
+  } if (  newValue > maxValue )  return
+       
+  }
+  const reset = () => {
+    setCount (0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AppWrapper" >
+      <div className='wrapper'> 
+      <div className='countWrapper'>
+        <div className= {count === maxValue ? 'error' : "notError"}>{count}</div>
+        </div>
+      <div className='button-box'>
+        <button className={count < maxValue ? "active" : "disable"} onClick={inc}>inc</button>
+        <button className={count === 0 ? "disable" : "active"} onClick={reset}>reset</button>
+      </div>
+      </div>
     </div>
   );
 }
